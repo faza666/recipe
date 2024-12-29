9f06e1a4b1b636a3a760c1e452a2f39b6bab7668
Dmytro Blinnikov <blinnikov.dmytro@gmail.com>
2024-12-29 13:15:03 +0200

    Fixed Flake8 errors

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -8,6 +8,7 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('Waiting for Database...')
@@ -17,7 +18,9 @@ class Command(BaseCommand):
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2Error, OperationalError):
-                self.stdout.write('Database is unavailable, waiting for 1 second...')
+                self.stdout.write(
+                    'Database is unavailable, waiting for 1 second...'
+                    )
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database is available!'))
